4a306b65b0be22203a24a242359b1b807b5ef341
Hōtarō Oreki <TheZjarek@gmail.com>
2022-01-27 13:17:39 +0100

    Lepszy wygląd strony gatunków

--- resources/views/breeds/index.blade.php
+++ resources/views/breeds/index.blade.php
@@ -1,4 +1,3 @@
-
 @extends('layouts.app')
 @section('title')
     Gatunki
@@ -10,35 +9,36 @@
             max-width: 90%;
         }
 
+        img {
+            max-height: 300px;
+            object-fit: cover;
+        }
+
     </style>
 @endsection
 @section('content')
-<div class="text-center">
-    <a class="btn btn-primary" href="{{ route('breeds/create') }}">Dodaj gatunek</a>
-    <hr>
-    @if (count($breeds) == 0)
-        <p>Nie ma gatunków do wyświetlenia.</p>
-    @else
-    <table>
-        <tr>
-            <th>Nazwa</th>
-            <th>Obrazek</th>
-            <th>Zakres ph</th>
-        </tr>
-        @foreach ($breeds as $breed)
-            <tr>
-                <td>
-                    <a>{{ $breed->name }}</a>
-                </td>
-                <td>
-                    <img src="{{ $breed->image_link }}" height="100">
-                </td>
-                <td>
-                    <a>{{ $breed->phMin }} - {{ $breed->phMax }}</a>
-                </td>
-            </tr>
-        @endforeach
-    </table>
-    @endif
-</div>
+    <div class="text-center">
+        <a class="btn btn-primary" href="{{ route('breeds/create') }}">Dodaj gatunek</a>
+        <hr>
+        @if (count($breeds) == 0)
+            <p>Nie ma gatunków do wyświetlenia.</p>
+        @else
+
+            <div class="container">
+                <div class="row row-cols-1 row-cols-md-3 g-4">
+                    @foreach ($breeds as $breed)
+                        <div class="col-xl-3 col-lg-4 col-sm-6 my-2">
+                            <div class="card h-100 border-primary">
+                                <img src="{{ $breed->image_link }}" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <h5 class="card-title">{{ $breed->name }}</h5>
+                                    <p class="card-text">Dozwolone ph: {{ $breed->phMin }} - {{ $breed->phMax }}</p>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+        @endif
+    </div>
 @endsection
