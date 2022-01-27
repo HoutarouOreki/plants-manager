4ccc57bc6bfed680022e47f7479a7687258d1395
Hōtarō Oreki <TheZjarek@gmail.com>
2022-01-27 13:39:12 +0100

    Tłumaczenie

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('Zalogowano!') }}
                 </div>
             </div>
         </div>
